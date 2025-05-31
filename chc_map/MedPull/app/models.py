e09cb671f0b40459506c56f6f0e04a26b44a6f1d
Ethan <71947274+EthanZhang87@users.noreply.github.com>
2025-05-31 13:40:21 -0400

    added django code

---
+++ chc_map/MedPull/app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    firstName = models.CharField(max_length=200)
+    middleName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user
